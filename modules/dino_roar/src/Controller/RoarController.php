568b48ac5236b891e5e355f7eb3383170beab0c2
Ryan Weaver <ryan@thatsquality.com>
2015-11-12 08:13:12 -0500

    Service: Inject keyvalue into service

--- modules/dino_roar/src/Controller/RoarController.php
+++ modules/dino_roar/src/Controller/RoarController.php
@@ -35,11 +35,7 @@ class RoarController extends ControllerBase
 
     public function roar($count)
     {
-        $keyValueStore = $this->keyValue('dino');
-
-        //$roar = $this->roarGenerator->getRoar($count);
-        //$keyValueStore->set('roar_string', $roar);
-        $roar = $keyValueStore->get('roar_string');
+        $roar = $this->roarGenerator->getRoar($count);
         $this->loggerFactory->get('default')
             ->debug($roar);
 
--- modules/dino_roar/src/Jurassic/RoarGenerator.php
+++ modules/dino_roar/src/Jurassic/RoarGenerator.php
@@ -2,10 +2,31 @@
 
 namespace Drupal\dino_roar\Jurassic;
 
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+
 class RoarGenerator
 {
+    private $keyValueFactory;
+
+    public function __construct(KeyValueFactoryInterface $keyValueFactory)
+    {
+        $this->keyValueFactory = $keyValueFactory;
+    }
+
     public function getRoar($length)
     {
-        return 'R'.str_repeat('O', $length).'AR!';
+        $store = $this->keyValueFactory->get('dino');
+        $key = 'roar_'.$length;
+
+        if ($store->has($key)) {
+            return $store->get($key);
+        }
+
+        sleep(2);
+
+        $string = 'R'.str_repeat('O', $length).'AR!';
+        $store->set($key, $string);
+
+        return $string;
     }
 }
