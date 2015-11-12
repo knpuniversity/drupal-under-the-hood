fd80ceae27e23be93a6788487d274f5a7d737344
Ryan Weaver <ryan@thatsquality.com>
2015-11-12 08:13:10 -0500

    Service: Register and use

--- modules/dino_roar/src/Controller/RoarController.php
+++ modules/dino_roar/src/Controller/RoarController.php
@@ -2,15 +2,33 @@
 
 namespace Drupal\dino_roar\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-class RoarController
+class RoarController extends ControllerBase
 {
+    /**
+     * @var RoarGenerator
+     */
+    private $roarGenerator;
+
+    public function __construct(RoarGenerator $roarGenerator)
+    {
+        $this->roarGenerator = $roarGenerator;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        $roarGenerator = $container->get('dino_roar.roar_generator');
+
+        return new static($roarGenerator);
+    }
+
     public function roar($count)
     {
-        $roarGenerator = new RoarGenerator();
-        $roar = $roarGenerator->getRoar($count);
+        $roar = $this->roarGenerator->getRoar($count);
 
         return new Response($roar);
     }
