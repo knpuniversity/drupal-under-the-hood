bc86539581042c4e880a5b66558ee9d21595f247
Ryan Weaver <ryan@thatsquality.com>
2015-11-12 08:13:16 -0500

    Event: Inject logger

--- modules/dino_roar/src/Jurassic/DinoListener.php
+++ modules/dino_roar/src/Jurassic/DinoListener.php
@@ -2,19 +2,28 @@
 
 namespace Drupal\dino_roar\Jurassic;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class DinoListener implements EventSubscriberInterface
 {
+    private $loggerChannel;
+
+    public function __construct(LoggerChannelFactoryInterface $loggerChannel)
+    {
+        $this->loggerChannel = $loggerChannel;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
         $shouldRoar = $request->query->get('roar');
 
         if ($shouldRoar) {
-            var_dump('ROOOOOOOOAR');die;
+            $channel = $this->loggerChannel->get('default')
+                ->debug('ROOOOOOOOAR');
         }
     }
 
